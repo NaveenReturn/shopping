1f9f8c6e67e3a9cd24b507d817e749ad202ac3c8
NaveenReturn <nknaveen8075@gmail.com>
2024-08-03 13:01:01 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
      <Header cart={cart}/>
       <div className="container">
           <Routes>
-              <Route path='/shopping/' element={<Home  />} />
+              <Route path='/' element={<Home  />} />
               <Route path='/cart' element={<Viewcart  />} />
           </Routes>
       </div>
